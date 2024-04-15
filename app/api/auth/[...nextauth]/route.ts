47460b1d74125dd7594b530f86ea0a6650daef64
Contactsushil <contact.sushilpandey@gmail.com>
2024-04-15 17:16:18 +0530

    Corrected

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,42 +1,45 @@
-import NextAuth from "next-auth";
-import type { AuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from 'next-auth';
+import type { AuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+// Define the authentication options
 export const authOptions: AuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GG_ID as string,
-      clientSecret: process.env.GG_SECRET as string,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET as string,
-  callbacks: {
-    async signIn({ account, profile }) {
-      if (account?.provider === 'google') {
-        const email = profile?.email || "";
-        const existingUser = await prisma.user.findUnique({
-          where: { email: email },
-        });
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GG_ID as string,
+            clientSecret: process.env.GG_SECRET as string,
+        }),
+    ],
+    secret: process.env.NEXTAUTH_SECRET as string,
+    callbacks: {
+        async signIn({ account, profile }) {
+            if (account?.provider === 'google') {
+                const email = profile?.email || '';
+                const existingUser = await prisma.user.findUnique({
+                    where: { email },
+                });
 
-        if (!existingUser) {
-          await prisma.user.create({
-            data: {
-              name: profile?.name,
-              email: email,
-            },
-          });
-        }
+                if (!existingUser) {
+                    await prisma.user.create({
+                        data: {
+                            name: profile?.name,
+                            email,
+                        },
+                    });
+                }
 
-        return true;
-      }
-      return true;
+                return true;
+            }
+            return true;
+        },
     },
-  },
 };
 
+// Create the handler with NextAuth
 const handler = NextAuth(authOptions);
 
+// Export the handler as the GET and POST methods of the API route
 export { handler as GET, handler as POST };
